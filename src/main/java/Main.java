c324f90fa8a23c737dc26efd36e2ec4b6be20532
daviidy <yaodavidarmel@gmail.com>
2025-04-21 18:34:54 -0500

    canning: Assignment & equality Operators

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,76 +3,98 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Main {
-  enum TokenType {
-      LEFT_PAREN,
-      RIGHT_PAREN,
-      LEFT_BRACE,
-      RIGHT_BRACE,
-      COMMA,
-      DOT,
-      MINUS,
-      PLUS,
-      SEMICOLON,
-      STAR
-  }
-  static boolean hadError = false;
-  public static void main(String[] args) {
-    System.err.println("Logs from your program will appear here!");
-
-    if (args.length < 2) {
-      System.err.println("Usage: ./your_program.sh tokenize <filename>");
-      System.exit(1);
+    enum TokenType {
+        LEFT_PAREN,
+        RIGHT_PAREN,
+        LEFT_BRACE,
+        RIGHT_BRACE,
+        COMMA,
+        DOT,
+        MINUS,
+        PLUS,
+        SEMICOLON,
+        STAR,
+        EQUAL,
+        EQUAL_EQUAL,
     }
 
-    String command = args[0];
-    String filename = args[1];
+    static boolean hadError = false;
+    private static int current = 0;
+    private static String lineLength;
 
-    if (!command.equals("tokenize")) {
-      System.err.println("Unknown command: " + command);
-      System.exit(1);
-    }
+    public static void main(String[] args) {
+        System.err.println("Logs from your program will appear here!");
 
-    String fileContents = "";
-    StringBuilder result = new StringBuilder();
-    try {
-        int lineNumber = 1;
-        for (String line: Files.readAllLines(Path.of(filename))) {
-          for(int i = 0; i < line.length(); i++) {
-            char c = line.charAt(i);
-            TokenType tokenType = getTokenType(c, lineNumber);
-            if (tokenType != null) {
-                System.out.println(tokenType + " " + c + " null");
-            } else {
-                hadError = true;
-                System.err.printf("[line %d] Error: Unexpected character: %c%n", lineNumber, c);
-            }
-          }
-          lineNumber++;
+        if (args.length < 2) {
+            System.err.println("Usage: ./your_program.sh tokenize <filename>");
+            System.exit(1);
         }
 
-    } catch (IOException e) {
-      System.err.println("Error reading file: " + e.getMessage());
-      System.exit(1);
-    }
-    System.out.println("EOF  null");
-    if (hadError) {
-      System.exit(65);
-    }
-  }
+        String command = args[0];
+        String filename = args[1];
 
-  private static TokenType getTokenType(char c, int lineNumber) {
-      return switch (c) {
-          case '(' -> TokenType.LEFT_PAREN;
-          case ')' -> TokenType.RIGHT_PAREN;
-          case '{' -> TokenType.LEFT_BRACE;
-          case '}' -> TokenType.RIGHT_BRACE;
-          case ',' -> TokenType.COMMA;
-          case '.' -> TokenType.DOT;
-          case '-' -> TokenType.MINUS;
-          case '+' -> TokenType.PLUS;
-          case ';' -> TokenType.SEMICOLON;
-          case '*' -> TokenType.STAR;
-          default -> null;
-      };
-  }
+        if (!command.equals("tokenize")) {
+            System.err.println("Unknown command: " + command);
+            System.exit(1);
+        }
+
+        try {
+            int lineNumber = 1;
+            for (String line : Files.readAllLines(Path.of(filename))) {
+                lineLength = line;
+                while (!isAtEnd()) {
+                    char c = line.charAt(current);
+                    TokenType tokenType = getTokenType(c, lineNumber);
+                    if (tokenType != null) {
+                        System.out.println(tokenType + " " + c + " null");
+                    } else {
+                        System.err.println("Error: Unexpected character '" + c + "' at line " + lineNumber);
+                        hadError = true;
+                    }
+                    advance();
+                }
+                lineNumber++;
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            System.exit(1);
+        }
+        System.out.println("EOF  null");
+        if (hadError) {
+            System.exit(65);
+        }
+    }
+
+    private static TokenType getTokenType(char c, int lineNumber) {
+        return switch (c) {
+            case '(' -> TokenType.LEFT_PAREN;
+            case ')' -> TokenType.RIGHT_PAREN;
+            case '{' -> TokenType.LEFT_BRACE;
+            case '}' -> TokenType.RIGHT_BRACE;
+            case ',' -> TokenType.COMMA;
+            case '.' -> TokenType.DOT;
+            case '-' -> TokenType.MINUS;
+            case '+' -> TokenType.PLUS;
+            case ';' -> TokenType.SEMICOLON;
+            case '*' -> TokenType.STAR;
+            case '=' -> {
+                if (lineLength.charAt(current + 1) == '=') {
+                    advance();
+                    yield TokenType.EQUAL_EQUAL;
+                } else {
+                    yield TokenType.EQUAL;
+                }
+            }
+            default -> null;
+        };
+    }
+
+    private static void advance() {
+        current++;
+    }
+
+    private static boolean isAtEnd() {
+        return current >= lineLength.length();
+    }
 }
